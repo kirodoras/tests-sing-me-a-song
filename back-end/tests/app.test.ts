73672d8a41bdf8d53646ea8ed6feb6d42ca2ee6f
Mateus Figueiredo Pereira <figueiredo1497@gmail.com>
2022-09-20 23:30:52 -0300

    back-end: set test in route POST /recommendations, should retun 201

--- back-end/tests/app.test.ts
+++ back-end/tests/app.test.ts
@@ -1,6 +1,7 @@
 import app from "../src/app";
 import supertest from "supertest";
 import { prisma } from "../src/database";
+import { fakerRecommendation } from "./factories/fakerRecommendations";
 
 console.log(process.env.DATABASE_URL);
 
@@ -13,6 +14,15 @@ afterAll(async () => {
 
 const request = supertest(app);
 const PATH = "/recommendations";
+
+describe(`POST ${PATH}`, () => {
+  it("should return 201", async () => {
+    const body = fakerRecommendation;
+    const result = await request.post(`${PATH}`).send(body);
+    const status = result.status;
+    expect(status).toEqual(201);
+  });
+});
 
 describe(`GET ${PATH}`, () => {
   it("should return 200", async () => {
