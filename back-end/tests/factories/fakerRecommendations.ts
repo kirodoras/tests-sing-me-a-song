7ad06aa433bb48abf58b6f11fe31e5301a9951ab
Mateus Figueiredo Pereira <figueiredo1497@gmail.com>
2022-09-20 23:54:13 -0300

    back-end: set types in faker recommendations

--- back-end/tests/factories/fakerRecommendations.ts
+++ back-end/tests/factories/fakerRecommendations.ts
@@ -1,11 +1,12 @@
 import { faker } from "@faker-js/faker";
+import { CreateRecommendationData } from "../../src/services/recommendationsService";
 
-export const correct = {
+export const correct: CreateRecommendationData = {
   name: faker.lorem.word(2),
   youtubeLink: "https://youtu.be/gSaIkQ3Z0jc",
 };
 
-export const wrongLink = {
+export const wrongLink: CreateRecommendationData = {
   name: faker.lorem.word(2),
   youtubeLink: "https://XXXXXXXXXX/XXXXXXXXX",
 };
