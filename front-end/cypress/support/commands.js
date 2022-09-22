a3a75e05472f8b585e407fdcee4f3e0037e646fd
Mateus Figueiredo Pereira <figueiredo1497@gmail.com>
2022-09-22 00:49:36 -0300

    front-end: set command to create a recommendation

--- front-end/cypress/support/commands.js
+++ front-end/cypress/support/commands.js
@@ -1,3 +1,15 @@
+/* eslint-disable cypress/no-unnecessary-waiting */
 Cypress.Commands.add("resetDatabase", () => {
     cy.request("POST", "http://localhost:5008/test/reset");
+});
+
+Cypress.Commands.add("createRecommendation", (path, name) => {
+    cy.intercept("GET", `${path}/recommendations`).as("getPosts");
+    const linkName = name;
+    cy.get('[data-cy="name"]').type(linkName);
+    cy.get('[data-cy="youtubeLink"]').type("https://youtu.be/Tu4sXwpY6S0");
+    cy.get('[data-cy="submitNew"]').click();
+    cy.wait("@getPosts");
+    cy.get('[data-cy="linkName"]').should('contain', linkName);
+    cy.wait(500);
 });
