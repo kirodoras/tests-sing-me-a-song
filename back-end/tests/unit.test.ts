b4a06390a009e62912817d843a15f726c74a6c21
Mateus Figueiredo Pereira <figueiredo1497@gmail.com>
2022-09-22 17:17:51 -0300

    back-end: set unit test to insert function in recommendationService

--- back-end/tests/unit.test.ts
+++ back-end/tests/unit.test.ts
@@ -1,5 +1,40 @@
-describe("Unit tests", () => {
-  it("should pass this test", () => {
-    expect(true).toBe(true);
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { recommendationRepository } from "../src/repositories/recommendationRepository";
+import { recommendationService } from "../src/services/recommendationsService";
+import * as fakerRecommendations from "./factories/fakerRecommendations";
+
+beforeEach(() => {
+  jest.clearAllMocks();
+  jest.resetAllMocks();
+});
+
+describe("Unit tests INSERT function in recommendationService", () => {
+  it("should pass to create", async () => {
+    const recommendation = fakerRecommendations.correct;
+    jest
+      .spyOn(recommendationRepository, "findByName")
+      .mockImplementationOnce((): any => {
+        return false;
+      });
+    jest
+      .spyOn(recommendationRepository, "create")
+      .mockImplementationOnce(async () => {
+        recommendation;
+      });
+    const result = await recommendationService.insert(recommendation);
+    expect(result).toBe(undefined);
+  });
+  it("should pass to error 409, conflict", async () => {
+    const recommendation = fakerRecommendations.correct;
+    jest
+      .spyOn(recommendationRepository, "findByName")
+      .mockImplementationOnce((): any => {
+        return true;
+      });
+    try {
+      await recommendationService.insert(recommendation);
+    } catch (err) {
+      expect(err.type).toBe("conflict");
+    }
   });
 });
